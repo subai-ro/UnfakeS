8242e8080f75ce17cb4cd4e5b0bcd789d4b844a8
subai-ro <inikita.silivestrov@gmail.com>
2025-03-22 01:49:22 +0100

    Add database table creation on startup

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from app import app
 from waitress import serve
+from schema_creation import create_tables
 
 if __name__ == "__main__":
+    # Create database tables on startup
+    create_tables()
     serve(app, host='0.0.0.0', port=10000) 
