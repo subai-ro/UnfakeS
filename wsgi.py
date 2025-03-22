a2ebfd1bd8f7ad5581eb90600164cd9593587aef
subai-ro <inikita.silivestrov@gmail.com>
2025-03-22 01:52:07 +0100

    Fix schema creation function name in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app import app
 from waitress import serve
-from schema_creation import create_tables
+from schema_creation import create_schema
 
 if __name__ == "__main__":
     # Create database tables on startup
-    create_tables()
+    create_schema()
     serve(app, host='0.0.0.0', port=10000) 
